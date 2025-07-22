1fd59cc028d8cae2097dbbe395d71299582c4b91
Adham167 <adhamabdelsalam3040@gmail.com>
2025-07-22 16:10:57 +0300

    product details screen done

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,14 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:scholar_shopping_app/models/cart_model.dart';
 import 'package:scholar_shopping_app/models/product_model.dart';
+import 'package:scholar_shopping_app/services/list_carts.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   const ProductDetailsScreen({super.key, required this.productModel});
   final ProductModel productModel;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      backgroundColor: Colors.blueAccent,
-      centerTitle: true,
-      title: Text(productModel.name,)));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        title: Text(productModel.name),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.asset(productModel.imageurl),
+            ),
+            SizedBox(height: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  productModel.name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "\$${productModel.price}",
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text("${productModel.description}"),
+                SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    final item = CartModel(
+                      1,
+                      price: productModel.price,
+                      name: productModel.name,
+                      image: productModel.imageurl,
+                    );
+
+                    listCarts.add(item);
+                  },
+                  child: Container(
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+
+                    child: Center(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 6),
+                          Text(
+                            "Add to Cart",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+
+                Text(
+                  "Product Details",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(productModel.productDetails),
+                SizedBox(height: 16),
+
+                Text(
+                  "Customer Reviews",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(productModel.customerReview),
+                SizedBox(height: 16),
+
+                Text(
+                  "Frequently Asked questions",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                SizedBox(height: 8),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: productModel.questionAnswers.length,
+                  itemBuilder: (context, index) {
+                    final qa = productModel.questionAnswers[index];
+                    return Card(
+                      elevation: 2,
+                      margin: EdgeInsets.symmetric(vertical: 6),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Q: ${qa.question}',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 4),
+                            Text('A: ${qa.answer}'),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 16),
+
+                Text(
+                  "Shopping Information",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(productModel.shoppingInformation),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
